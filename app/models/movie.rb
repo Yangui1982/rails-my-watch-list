f0580c786bdbdbf999afe8d5fdb162ce44ec961c
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-11-16 22:08:03 +0100

    model corrections

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,5 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, uniqueness: true
   validates :title, presence: true
-  validates :overview, uniqueness: true
   validates :overview, presence: true
 end
